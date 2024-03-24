d87bf7c6c945e099f4e0889826651011e3815e10
MCchaca <164785471+MCchaca@users.noreply.github.com>
2024-03-24 11:57:54 -0700

    Add files via upload

---
+++ personal_info_input_output.java
@@ -0,0 +1,22 @@
+package personal_info_mod1;
+//not able to figure out how to import scanner, will just do raw input 
+public class personal_info_input_output {
+
+	public static void main(String[] args) {
+		//define the personal inputs
+		String first = "Bob";
+		String last = "Billy";
+		String address = "123 tinstafl lane";
+		String city = "moon";
+		String zip = "424242";
+		
+		//print the personal info on seperate lines
+		//use ctrl + alt + down to copy a line down
+		System.out.println("First name: " + first);
+		System.out.println("Last name: " + last);
+		System.out.println("Address: " + address);
+		System.out.println("City: " + city);
+		System.out.println("Zipcode: " + zip);
+	}
+
+}
